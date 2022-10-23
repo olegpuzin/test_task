a7558e840a8e03dc2fb98656b760151ec738ee88
olegpuzin <olegpuzin77@icloud.com>
2022-10-23 23:50:48 +0300

    create_pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
+import Joke from './components/Joke';
+import Pagination from './components/Pagination';
+import Spinner from './components/Spinner';
 
 function App() {
+  const [jokes, setJokes] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const [page, setPage] = useState(1);
+  const [countPage, setCountPage] = useState(0);
+
+  const limitPage = 10;
+
+  useEffect(() => {
+    
+    const getJokes = async() => {
+      try {
+        await axios(`https://icanhazdadjoke.com/search?page=${page}&limit=${limitPage}`, {headers:{'Accept': 'application/json'}})
+        .then(({data}) => {
+          setJokes(data.results);
+          setCountPage(data.total_pages);
+      })
+      setLoader(false);
+      } catch (err) {
+        alert('Ошибка при получении данных');
+        console.log(err)
+      }
+    }
+
+    getJokes();
+  }, [page]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='content'>
+        {loader 
+        ? <div className='spinner'><Spinner /></div>
+        : jokes.map(({ id, joke }) => (
+            <Joke key={id} joke={joke} />
+          ))}
+      </div>
+      <Pagination page={page} countPage={countPage} setPage={setPage} />
     </div>
   );
 }
---
+++ src/components/Joke.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Joke({ joke }) {
+    return (
+        <div className='joke'>
+            {joke}
+        </div>
+    )
+}
+
+export default Joke;
